dceacb8bf63873ef730067727134577e4833532c
Hariharan Anbu <anbu8100@mylaurier.ca>
2024-11-21 16:52:55 -0500

    Hariharan

--- app/src/main/java/com/wlu/tourguys/project/TestActivity.java
+++ app/src/main/java/com/wlu/tourguys/project/TestActivity.java
@@ -1,4 +1,32 @@
 package com.wlu.tourguys.project;
 
-public class TestActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test); // Create a simple layout with buttons
+
+        Button contactButton = findViewById(R.id.contact_details_button);
+        Button addTripButton = findViewById(R.id.add_trip_button);
+
+        contactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TestActivity.this, ContactDetailsActivity.class));
+            }
+        });
+
+        addTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TestActivity.this, AddTripActivity.class));
+            }
+        });
+    }
 }
